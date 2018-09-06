2a2d4dd3a3a949f456cf73e5c621491fbe91b15e
Khang Dinh <thesephi@gmail.com>
2018-09-06 03:33:41 +0200

    Make it JustWork (TM)

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,31 @@
 // - check for the existence of a `.gitignore` file already
 // - setting destination output (right now it's the same dest as `process.cwd()`)
 // - remote content request failover (e.g. due to network issues)
+// - `https.get` error handling
 
 // imports
-import http from "http"
+import https from "https"
 import fs from "fs"
 import path from "path"
+import { IncomingMessage } from "http"
 
 const destinationFile: string = path.join(process.cwd(), ".gitignore")
-console.log(`writing ${destinationFile}`)
 
 // step 1: grab the file from GitHub
-http.get("http://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore")
-  .pipe(fs.createWriteStream(destinationFile))
+// http://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore
+const sourceFileURI: string = "https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore"
+let data: string = ""
+https.get(sourceFileURI, (res: IncomingMessage): void => {
+
+  res.on("data", (chunk: any): void => {
+    data += chunk
+  })
+
+  // step 2: write result to destination file
+  res.on("end", () => {
+    console.log(`writing ${destinationFile} ...`)
+    fs.writeFile(destinationFile, data, (err: Error): void =>
+      console.error(err ? err : "done\n"))
+  })
+
+})
