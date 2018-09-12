cdc300fb266fd31b3d9fd9a59aefa52da4172051
Khang Dinh <thesephi@gmail.com>
2018-09-13 00:12:56 +0200

    Updated build process, utilising webpack

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 
 /**
  * Message for contributors: please KISS
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require("webpack");
+
+module.exports = {
+  plugins: [
+    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true })
+  ]
+}
